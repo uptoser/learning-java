177d30528e1ca375999855e43f1decf7e4212eea
uptoser <450052550@qq.com>
2024-12-13 10:39:00 +0800

    增加了DateFormat

--- javase/src/main/java/com/uptoser/java/javase/baselib/DateMain.java
+++ javase/src/main/java/com/uptoser/java/javase/baselib/DateMain.java
@@ -1,8 +1,13 @@
 package com.uptoser.java.javase.baselib;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
+import static java.text.DateFormat.FULL;
+import static java.text.DateFormat.MEDIUM;
 import static java.util.Calendar.*;
 
 public class DateMain {
@@ -102,6 +107,72 @@ public class DateMain {
         //设置DATE字段为5
         cal.set(DATE , 5);    //②
         System.out.println(cal.getTime());    //③
+    }
+
+    /**
+     * DateFormat也是一个抽象类，它也提供了如下几个类方法用于获取DateFormat对象。
+     * ➢ getDateInstance()：返回一个日期格式器，它格式化后的字符串只有日期，没有时间。
+     * 该方法可以传入多个参数，用于指定日期样式和Locale等参数；如果不指定这些参数，则使用默认参数。
+     * ➢ getTimeInstance()：返回一个时间格式器，它格式化后的字符串只有时间，没有日期。
+     * 该方法可以传入多个参数，用于指定时间样式和Locale等参数；如果不指定这些参数，则使用默认参数。
+     * ➢ getDateTimeInstance()：返回一个日期、时间格式器，它格式化后的字符串既有日期，也有时间。
+     * 该方法可以传入多个参数，用于指定日期样式、时间样式和Locale等参数；如果不指定这些参数，则使用默认参数。
+     */
+    private void dateFormat() throws ParseException {
+//需要被格式化的时间
+        Date dt = new Date();
+        //创建两个Locale，分别代表中国、美国
+        Locale[] locales = {Locale.CHINA, Locale.US};
+        DateFormat[] df = new DateFormat[16];
+        //为上面两个Locale创建16个DateFormat对象
+        for (int i = 0 ; i < locales.length ; i++)
+        {
+            df[i * 8] = DateFormat.getDateInstance(SHORT, locales[i]);
+            df[i * 8 + 1] = DateFormat.getDateInstance(MEDIUM, locales[i]);
+            df[i * 8 + 2] = DateFormat.getDateInstance(LONG, locales[i]);
+            df[i * 8 + 3] = DateFormat.getDateInstance(FULL, locales[i]);
+            df[i * 8 + 4] = DateFormat.getTimeInstance(SHORT, locales[i]);
+            df[i * 8 + 5] = DateFormat.getTimeInstance(MEDIUM , locales[i]);
+            df[i * 8 + 6] = DateFormat.getTimeInstance(LONG , locales[i]);
+            df[i * 8 + 7] = DateFormat.getTimeInstance(FULL , locales[i]);
+        }
+        for (int i = 0 ; i < locales.length ; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    System.out.println("-------中国日期格式--------");
+                    break;
+                case 1:
+                    System.out.println("-------美国日期格式--------");
+                    break;
+            }
+            System.out.println("SHORT格式的日期格式："
+                    + df[i * 8].format(dt));
+            System.out.println("MEDIUM格式的日期格式："
+                    + df[i * 8 + 1].format(dt));
+            System.out.println("LONG格式的日期格式："
+                    + df[i * 8 + 2].format(dt));
+            System.out.println("FULL格式的日期格式："
+                    + df[i * 8 + 3].format(dt));
+            System.out.println("SHORT格式的时间格式："
+                    + df[i * 8 + 4].format(dt));
+            System.out.println("MEDIUM格式的时间格式："
+                    + df[i * 8 + 5].format(dt));
+            System.out.println("LONG格式的时间格式："
+                    + df[i * 8 + 6].format(dt));
+            System.out.println("FULL格式的时间格式："
+                    + df[i * 8 + 7].format(dt));
+        }
+
+        String str1 = "2007-12-12";
+        String str2 = "2007年12月10日";
+        //下面输出 Wed Dec 12 00:00:00 CST 2007
+        System.out.println(DateFormat.getDateInstance().parse(str1));
+        //下面输出 Mon Dec 10 00:00:00 CST 2007
+        System.out.println(DateFormat.getDateInstance(LONG).parse(str2));
+        //下面抛出 ParseException异常
+        System.out.println(DateFormat.getDateInstance().parse(str2));
     }
 
     /*
