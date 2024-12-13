d78dc1908d98850ba3517fcff5e10e719302d5e6
uptoser <450052550@qq.com>
2024-12-13 11:18:16 +0800

    增加了DateTimeFormatter

--- javase/src/main/java/com/uptoser/java/javase/baselib/DateMain.java
+++ javase/src/main/java/com/uptoser/java/javase/baselib/DateMain.java
@@ -2,6 +2,7 @@ package com.uptoser.java.javase.baselib;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -191,7 +192,48 @@ public class DateMain {
     ➢ DayOfWeek：这是一个枚举类，定义了周日到周六的枚举值。
     ➢ Month：这也是一个枚举类，定义了一月到十二月的枚举值。
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         new DateMain().dateTest().calendarTest();
+        //SimpleDateFormat 创建SimpleDateFormat对象时需要传入一个pattern字符串 是一个日期模板字符串
+        Date d = new Date();
+        //创建一个SimpleDateFormat对象
+        SimpleDateFormat sdf1 = new SimpleDateFormat("Gyyyy年中第D天");
+        //将d格式化成日期，输出：公元2007年中第354天
+        String dateStr = sdf1.format(d);
+        System.out.println(dateStr);
+        //一个非常特殊的日期字符串
+        String str = "07###三月##21";
+        SimpleDateFormat sdf2 = new SimpleDateFormat("y###MMM##d");
+        //将日期字符串解析成日期，输出：Wed Mar 21 00:00:00 CST 2007
+        System.out.println(sdf2.parse(str));
+        SimpleDateFormat sdf3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf3.format(d));
+
+        /*
+        Letter	Date or Time Component	Presentation	Examples
+            G	Era designator	Text	AD
+            y	Year	Year	1996; 96
+            Y	Week year	Year	2009; 09
+            M	Month in year (context sensitive)	Month	July; Jul; 07
+            L	Month in year (standalone form)	Month	July; Jul; 07
+            w	Week in year	Number	27
+            W	Week in month	Number	2
+            D	Day in year	Number	189
+            d	Day in month	Number	10
+            F	Day of week in month	Number	2
+            E	Day name in week	Text	Tuesday; Tue
+            u	Day number of week (1 = Monday, ..., 7 = Sunday)	Number	1
+            a	Am/pm marker	Text	PM
+            H	Hour in day (0-23)	Number	0
+            k	Hour in day (1-24)	Number	24
+            K	Hour in am/pm (0-11)	Number	0
+            h	Hour in am/pm (1-12)	Number	12
+            m	Minute in hour	Number	30
+            s	Second in minute	Number	55
+            S	Millisecond	Number	978
+            z	Time zone	General time zone	Pacific Standard Time; PST; GMT-08:00
+            Z	Time zone	RFC 822 time zone	-0800
+            X	Time zone	ISO 8601 time zone	-08; -0800; -08:00
+         */
     }
 }
---
+++ javase/src/main/java/com/uptoser/java/javase/java8/newtime/DateTimeFormatterMain.java
@@ -0,0 +1,29 @@
+package com.uptoser.java.javase.java8.newtime;
+
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Arrays;
+
+/**
+ * DateTimeFormatter时间格式化工具
+ */
+public class DateTimeFormatterMain {
+    public static void main(String[] args) {
+        DateTimeFormatter[] dateTimeFormatters = {
+                //直接使用常量创建DateTimeFormatter
+                DateTimeFormatter.ISO_LOCAL_DATE,
+                DateTimeFormatter.ISO_LOCAL_TIME,
+                DateTimeFormatter.ISO_LOCAL_DATE_TIME,
+                //使用本地化的不同风格来创建 DateTimeFormatter 格式器
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL,FormatStyle.MEDIUM),
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG,FormatStyle.SHORT),
+                DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG),
+                DateTimeFormatter.ofPattern("Gyyyy年MM月dd日 HH:mm:ss")
+        };
+        LocalDateTime now = LocalDateTime.now();
+        //依次使用不同的格式器对 LocalDateTime 进行格式化
+        Arrays.stream(dateTimeFormatters).forEach(f-> System.out.println(f.format(now)));//也可以使用now.format(f)
+    }
+}
